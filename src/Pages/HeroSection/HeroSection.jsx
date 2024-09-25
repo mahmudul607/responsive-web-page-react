424bd6d530a5d0af46edf99584bfa1d9d619e871
unknown <mahmudul.cse.eng@gmail.com>
2024-09-25 17:20:50 +0600

    react spring animation is added

--- src/Pages/HeroSection/HeroSection.jsx
+++ src/Pages/HeroSection/HeroSection.jsx
@@ -1,13 +1,28 @@
+import { useSpring, animated } from '@react-spring/web';
 import './HeroSection.css'
 
+
+
+
 const HeroSection = () => {
+    const springs = useSpring({
+        from: { x: -400 },
+        to: { x: 0 },
+      })
+
     return (
         <section className="hero_section flex justify-center items-center max-[767.99px]:absolute max-[767.99px]:top-0 max-[767.99px]:-z-10 w-full max-[767.99px]:min-h-screen">
-        <div className="hero_content text-center text-white p-12 bg-slate-600 bg-opacity-25">
-            <h1 className='text-3xl'>Welcome To Visit Here</h1>
-            <p className='text-xl'>This is a single page website</p>
-        </div>
-    </section>
+
+            <animated.div style={springs}>
+                <div className="hero_content text-center text-white p-12 bg-slate-600 bg-opacity-25">
+                    <h1 className='text-3xl'>Welcome To Visit Here</h1>
+                    <p className='text-xl'>This is a single page website</p>
+                </div>
+            </animated.div>
+
+      
+
+        </section>
     );
 };
 
